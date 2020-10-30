5a5247480e08ddef63ea45c9d69440481944668b
(Vinícius <(viniciuss.sync@gmail.com)>
2020-10-30 11:10:59 -0300

    Resolvendo todos os testes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api.js';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+useEffect(() => {
+  api.get('repositories').then(response => {
+    setRepositories(response.data);
+  });
+}, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'PS4',
+      url: 'https://github.com',
+      techs: ['Redux', 'Axios']
+    })
+  
+    setRepositories([... repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+   await api.delete(`repositories/${id}`);
+
+   setRepositories(repositories.filter(
+     repository => repository.id != id
+   ))
+  
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
